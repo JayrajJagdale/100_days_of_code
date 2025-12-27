fc7ce5e7d84b5204635cd9aa9b10a47567f1bffe
JayrajJagdale <jayrajj2029@gmail.com>
2025-12-27 19:00:23 +0530

    Day 1, Day 2 and Day 3 added

--- Day2/task.py
+++ Day2/task.py
@@ -1,31 +1,31 @@
 # Subscripting
-# print("Hello"[1])
+print("Hello"[1])
 
 # String
-# print("123" + "456")
+print("123" + "456")
 
 # Integer = Whole number
-# print(123 + 456)
+print(123 + 456)
 
 # Large Integer
-# print(123_456_789)
+print(123_456_789)
 
 # Float = Floating Point Number
-# print(3.142524)
+print(3.142524)
 
 # Boolean
-# print(True)
-# print(False)
+print(True)
+print(False)
 
-# print(type("Hello"))
-# print(type(1234))
-# print(type(24.7383))
-# print(type(True))
+print(type("Hello"))
+print(type(1234))
+print(type(24.7383))
+print(type(True))
 
 # Conversion
-# print(int("123") + int("456"))
+print(int("123") + int("456"))
 
-#print("Number of letters in your name:" + str(len(input("Enter Your name"))))
+print("Number of letters in your name:" + str(len(input("Enter Your name"))))
 
 # Mathematical Operations
 
@@ -42,19 +42,19 @@ print(2 ** 3)
 # * OR /
 # + OR -
 
-# print(3 * 3 + 3 / 3 - 3 )
-# print(3 * (3 + 3) / 3 -3)
+print(3 * 3 + 3 / 3 - 3 )
+print(3 * (3 + 3) / 3 -3)
 
-# bmi = 84 / 1.65 ** 2
-# print(bmi)
+bmi = 84 / 1.65 ** 2
+print(bmi)
 
-# print(int(bmi))
+print(int(bmi))
 
-# print(round(bmi))
+print(round(bmi))
 
-# print(round(bmi, 2))
+print(round(bmi, 2))
 
-# score = 0
-# height = 6.6
-# print(f"Your score = {score} and height is {height}")
+score = 0
+height = 6.6
+print(f"Your score = {score} and height is {height}")
 
