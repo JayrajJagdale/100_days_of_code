f16138dbb1da34f8db673ab46025e5e8f3a4c5fd
JayrajJagdale <jayrajj2029@gmail.com>
2026-01-11 12:14:02 +0530

    Day 13

---
+++ Day13/describe.py
@@ -0,0 +1,11 @@
+def my_function():
+    for i in range(1, 20): # change 20 to 21 it works
+        if i == 20:
+            print("You got it")
+
+my_function()
+
+# Describe the Problem - Write you answers as comments:
+# 1. What is the for loop doing?
+# 2. What is the function meant to print "You got it"?
+# 3. What are your assumptions about the value of i?
---
+++ Day13/reproduce_bug.py
@@ -0,0 +1,6 @@
+from random import randint
+dic_images = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+# dice_num = randint(1,6)
+# dice_num = 6  # It always shows error 
+dice_num = randint(0,5)
+print(dic_images[dice_num])
